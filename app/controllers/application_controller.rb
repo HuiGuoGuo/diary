0675324fc68d9a535625347e4dc9631d151adb98
wanghui <wanghui@email.com>
2014-12-15 14:29:57 +0800

    12.15

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_filter :set_locale
   protect_from_forgery
+
+  def set_locale
+    # 可以將 ["en", "zh-TW"] 設定為 VALID_LANG 放到 config/environment.rb 中
+    if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
+      session[:locale] = params[:locale]
+    end
+
+    I18n.locale = session[:locale] || I18n.default_locale
+  end			
+
   def after_sign_in_path_for(resource)
     if resource.is_a?(User)
       if User.count == 1
